7e3f809038250fa3f9a92e831269bdff0e06d54c
shamim <shamimsajedidev@gmail.com>
2024-06-18 16:43:35 +0330

    feat
    add Button(atom)

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -1,5 +1,12 @@
+import { Button } from "@/ui/atom";
+
 function LoginPage() {
-  return <div>LoginPage</div>;
+  return (
+    <div className="container mx-auto">
+      LoginPage
+      <Button name="emoji" isRoundedFull  >login</Button>
+    </div>
+  );
 }
 
 export default LoginPage;
