34fd5b53369fd7b9719ad02b077da855f4362b81
sabourifar <mh.sabourifar@gmail.com>
2024-06-19 18:22:13 +0330

    New Typography Method

--- src/ui/atom/typography/HeadingXS.tsx
+++ src/ui/atom/typography/HeadingXS.tsx
@@ -1,21 +1,88 @@
-import { colorClassMap, type TypoColor } from "./TypoColors";
+import type React from "react";
 
-interface Props {
+const colorClassMap = {
+	white: "text-white",
+	gray: "text-[#818181]",
+	black: "text-[#1E1E1E]",
+	brandPrimary: "text-brand-primary",
+	gradient:
+		"inline-block text-transparent bg-clip-text bg-gradient-to-r from-hgradient-primary to-hgradient-secondary",
+};
+
+type TypoColor = "white" | "gray" | "black" | "brandPrimary" | "gradient";
+
+interface TextProps {
 	children: string;
 	color: TypoColor;
+	variant: "body" | "bold" | "heading";
+	size: "XS" | "S" | "M" | "L" | "XL";
 }
 
-const HeadingXS: React.FC<Props> = ({ children, color }) => {
+type SizeMap = {
+	[key in "XS" | "S" | "M" | "L" | "XL"]?: string;
+};
+
+const sizeMap: { [key in TextProps["variant"]]: SizeMap } = {
+	body: {
+		XS: "12px",
+		S: "14px",
+		M: "16px",
+		L: "20px",
+		XL: "24px",
+	},
+	bold: {
+		XS: "12px",
+		S: "14px",
+		M: "16px",
+		L: "20px",
+		XL: "24px",
+	},
+	heading: {
+		XS: "20px",
+		S: "24px",
+		M: "28px",
+		L: "32px",
+	},
+};
+
+type ElementMap = {
+	body: "p";
+	bold: "b";
+	heading: {
+		XS: "h4";
+		S: "h3";
+		M: "h2";
+		L: "h1";
+	};
+};
+
+const elementMap: ElementMap = {
+	body: "p",
+	bold: "b",
+	heading: {
+		XS: "h4",
+		S: "h3",
+		M: "h2",
+		L: "h1",
+	},
+};
+
+const Text: React.FC<TextProps> = ({ children, color, variant, size }) => {
 	const colorClass = colorClassMap[color];
+	const fontSize = sizeMap[variant][size];
+	const Element =
+		variant === "heading"
+			? elementMap.heading[size as keyof ElementMap["heading"]]
+			: elementMap[variant];
 
 	return (
-		<h4
-			className={`font-yekan-bold ${colorClass}`}
-			style={{ fontSize: "20px" }}
+		<Element
+			className={`font-yekan-${variant === "bold" ? "bold" : "normal"} ${colorClass}`}
+			style={{ fontSize }}
 		>
 			{children}
-		</h4>
+		</Element>
 	);
 };
 
-export default HeadingXS;
+export default Text;
