64dd56719baad49d24d4f858563a16993560e347
shamim <shamimsajedidev@gmail.com>
2024-06-19 21:55:17 +0330

    refactor :type props Button

--- src/ui/atom/Button/index.tsx
+++ src/ui/atom/Button/index.tsx
@@ -4,29 +4,41 @@ import BodyXS from "../typography/BodyXS";
 import BoldXS from "../typography/BoldXS";
 interface ButtonProps {
   onClick: () => void;
-  className: string;
-  children?: React.ReactNode;
-  isRoundedFull:boolean
-  isBold:boolean;
-  isSmallRoundedButton:boolean;
-  name:string;
-  colorClass:string;
-
+  className?: string;
+  children: React.ReactNode;
+  isLeftRounded?: boolean;
+  isBold?: boolean;
+  isSmallRoundedButton?: boolean;
+  iconName?: string;
 }
 
-export const Button = ({ onClick, className, children ,isBold,name,isRoundedFull,isSmallRoundedButton}: ButtonProps) => {
-
+export const Button = ({
+  onClick,
+  className,
+  children,
+  isBold,
+  iconName,
+  isLeftRounded,
+  isSmallRoundedButton,
+}: ButtonProps) => {
   return (
     <button
       className={`bg-brand-primary 
-        ${isRoundedFull ? isSmallRoundedButton ?"w-[125px] rounded-md" : "w-full rounded-md"  : "rounded-l-md w-[91px]"}
+        ${
+          isLeftRounded
+            ? "rounded-l-md w-[91px]"
+            : isSmallRoundedButton
+            ? "w-[125px] rounded-md"
+            : "w-full rounded-md"
+        }
          text-white p-2.5 gap-2 ${className}`}
       onClick={onClick}
     >
       <div className="flex justify-center items-center gap-1">
-        <SvgIcon name={name} />
+        {iconName && <SvgIcon name={iconName} />}
         {isBold ? <BoldXS>{children}</BoldXS> : <BodyXS>{children}</BodyXS>}
       </div>
     </button>
   );
 };
+
