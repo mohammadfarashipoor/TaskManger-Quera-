2f63f901e171ca2bfdc1adf0a921dbf0f0081d7c
Mohammad <mohammad1f2f@gmail.com>
2024-06-21 00:07:03 +0330

    feat : exmaple formLogin

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -1,5 +1,21 @@
+import InputText from "@/ui/atom/InputText";
+import FormContainer from "@/ui/molocol/FormContainer";
+import { schemaLogin } from "@/validation/validationSchema";
+interface LoginData {
+  username: string;
+  password: string;
+}
 function LoginPage() {
-  return <div>LoginPage</div>;
+  const onSubmit = (data: LoginData) => {
+    console.log(data);
+  };
+  return (
+    <FormContainer onSubmit={onSubmit} schema={schemaLogin}>
+      <InputText name="username" label="username" />
+      <InputText name="password" label="password" type="password" />
+      <button type="submit">Submit</button>
+    </FormContainer>
+  );
 }
 
 export default LoginPage;
