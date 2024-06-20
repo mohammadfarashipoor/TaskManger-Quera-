3d1fca6cebed86f21d73607f7412af67027a9739
Mohammad <mohammad1f2f@gmail.com>
2024-06-21 00:03:08 +0330

    feat : atom InputText component

---
+++ src/ui/atom/InputText/index.tsx
@@ -0,0 +1,30 @@
+import { useFormContext } from "react-hook-form";
+
+interface InputTextProps {
+  name: string;
+  classNames?: string;
+  label?: string;
+  type?: "text" | "number" | "password";
+}
+
+const InputText: React.FC<InputTextProps> = (props) => {
+  const { name, label, classNames, type = "text" } = props;
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+  return (
+    <div className="flex flex-col items-end gap-xs self-stretch">
+      {label && <label htmlFor={name}>{label}</label>}
+      <input
+        id={name}
+        type={type}
+        className={`p-xs self-stretch rounded-[6px] border border-gray-400 h-xl ${classNames} `}
+        {...register(name)}
+      />
+      {errors[name] && <span>{errors[name].message}</span>}
+    </div>
+  );
+};
+
+export default InputText;
