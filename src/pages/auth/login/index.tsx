de97cd7e047e51412cc522630444fec2bb6e36bc
Mohammad <mohammad1f2f@gmail.com>
2024-06-21 23:36:27 +0330

    refactor : exmaple formLogin

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -1,21 +1,5 @@
-import InputText from "@/ui/atom/InputText";
-import FormContainer from "@/ui/molocol/FormContainer";
-import { schemaLogin } from "@/validation/validationSchema";
-interface LoginData {
-  username: string;
-  password: string;
-}
 function LoginPage() {
-  const onSubmit = (data: LoginData) => {
-    console.log(data);
-  };
-  return (
-    <FormContainer onSubmit={onSubmit} schema={schemaLogin}>
-      <InputText name="username" label="username" />
-      <InputText name="password" label="password" type="password" />
-      <button type="submit">Submit</button>
-    </FormContainer>
-  );
+  return <div>LoginPage</div>;
 }
 
 export default LoginPage;
