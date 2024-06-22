907b7f03c97a7e95040d05f0fbab550212e5ca2b
mr-rahimi900 <mrrahimi.frontend@gmail.com>
2024-06-22 22:30:02 +0330

    style: add classname, props in login page

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -1,17 +1,17 @@
-import { Button } from "@/ui/atom";
+import { Button } from "@/ui/atom/Button";
 import InputText from "@/ui/atom/InputText";
+import Text from "@/ui/atom/typography/Text";
 import { OutBox } from "@/ui/molocol/Box";
 import FormContainer from "@/ui/molocol/FormContainer";
+import { Link } from "react-router-dom";
 
 function LoginPage() {
-
   const submitHandler = () => {
     console.log("clicked");
   };
   const clickHandler = () => {
     console.log("clicked");
   };
-
 
   return (
     <div>
@@ -21,8 +21,16 @@ function LoginPage() {
           title="به کوئرا تسک منیجر خوش برگشتی"
         >
           <InputText name="user" label="نام کاربری" classNames="mb-4" />
-          <InputText name="password" label="رمز عبور" classNames="mb-4" />
-          <Button onClick={clickHandler}>ورود</Button>
+          <InputText name="password" label="رمز عبور" classNames="mb-2" />
+          <Link to='/forgot' className="flex underline">رمز عبور خود را فراموش کرده اید؟</Link>
+          <Button className="mt-6" onClick={clickHandler} isBold>ورود</Button>
+          <div className="mt-4">
+            <Text>
+            ثبت نام نکرده ای؟
+            <Link className="text-brand-primary ms-2" to="/register">ثبت نام</Link>
+          </Text>
+          </div>
+          
         </OutBox>
       </FormContainer>
     </div>
