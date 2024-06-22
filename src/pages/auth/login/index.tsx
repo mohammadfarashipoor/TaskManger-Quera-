a8cd751f8fbf21c12b3bdc16b1538a044e892da6
mr-rahimi900 <mrrahimi.frontend@gmail.com>
2024-06-22 19:41:27 +0330

    feat(login page): add input , button atomes

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -1,8 +1,30 @@
+import { Button } from "@/ui/atom";
+import InputText from "@/ui/atom/InputText";
+import { OutBox } from "@/ui/molocol/Box";
+import FormContainer from "@/ui/molocol/FormContainer";
 
 function LoginPage() {
+
+  const submitHandler = () => {
+    console.log("clicked");
+  };
+  const clickHandler = () => {
+    console.log("clicked");
+  };
+
+
   return (
-    <div >
-      LoginPage
+    <div>
+      <FormContainer onSubmit={submitHandler} schema>
+        <OutBox
+          className="w-[640px] h-[429px] px-8"
+          title="به کوئرا تسک منیجر خوش برگشتی"
+        >
+          <InputText name="user" label="نام کاربری" classNames="mb-4" />
+          <InputText name="password" label="رمز عبور" classNames="mb-4" />
+          <Button onClick={clickHandler}>ورود</Button>
+        </OutBox>
+      </FormContainer>
     </div>
   );
 }
