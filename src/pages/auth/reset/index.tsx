d0e9e3d46ad2f38a8aafb6770e22012861036e77
shamim <shamimsajedidev@gmail.com>
2024-06-23 12:32:57 +0330

    fix: edit Button

--- src/pages/auth/reset/index.tsx
+++ src/pages/auth/reset/index.tsx
@@ -1,5 +1,37 @@
+import { Button } from "@/ui/atom/Button";
+import InputText from "@/ui/atom/InputText";
+import { OutBox } from "@/ui/molocol/Box";
+import FormContainer from "@/ui/molocol/FormContainer";
+
 function ResetPage() {
-  return <div>ResetPage</div>;
+  return (
+    <>
+      <div
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: " 50%",
+          transform: "translate(-50%, -50%)",
+        }}
+      >
+        <FormContainer>
+          <OutBox className="w-[640px] p-6 gap-8 flex flex-col ju" title="تغییر رمز عبور">
+            <InputText
+              label="رمز عبور جدید را وارد کنید"
+              name="password"
+              type="password"
+            ></InputText>
+            <InputText
+              label="تکرار رمز عبور"
+              name="confrimPassword"
+              type="password"
+            ></InputText>
+            <Button type="submit" isBold>اعمال تغییرات</Button>
+          </OutBox>
+        </FormContainer>
+      </div>
+    </>
+  );
 }
 
 export default ResetPage;
