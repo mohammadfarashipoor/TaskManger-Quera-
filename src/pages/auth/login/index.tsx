c58a5410b70c7b1492b53c1379709fcb912e13dd
mr-rahimi900 <mrrahimi.frontend@gmail.com>
2024-06-23 13:13:06 +0330

    fix : validate submit in login page

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -3,27 +3,30 @@ import InputText from "@/ui/atom/InputText";
 import Text from "@/ui/atom/typography/Text";
 import { OutBox } from "@/ui/molocol/Box";
 import FormContainer from "@/ui/molocol/FormContainer";
+import { schemaLogin } from "@/validation/validationSchema";
 import { Link } from "react-router-dom";
+interface LoginData {
+  username: string;
+  password: string;
+}
 
 function LoginPage() {
-  const submitHandler = () => {
-    console.log("clicked");
-  };
-  const clickHandler = () => {
-    console.log("clicked");
+
+  const onSubmit = (data:LoginData) => {
+    console.log("data" , data);
   };
 
   return (
     <div>
-      <FormContainer onSubmit={submitHandler} schema>
+      <FormContainer onSubmit={onSubmit} schema={schemaLogin}>
         <OutBox
           className="w-[640px] h-[429px] px-8"
           title="به کوئرا تسک منیجر خوش برگشتی"
         >
-          <InputText name="user" label="نام کاربری" classNames="mb-4" />
-          <InputText name="password" label="رمز عبور" classNames="mb-2" />
+          <InputText name="username" label="نام کاربری" classNames="mb-4" />
+          <InputText type="password" name="password" label="رمز عبور" classNames="mb-2" />
           <Link to='/forgot' className="flex underline">رمز عبور خود را فراموش کرده اید؟</Link>
-          <Button className="mt-6" onClick={clickHandler} isBold>ورود</Button>
+          <Button className="mt-6" type="submit" isBold>ورود</Button>
           <div className="mt-4">
             <Text>
             ثبت نام نکرده ای؟
