f907dbb793119345a421099f07f339c5777c9ac3
shamim <shamimsajedidev@gmail.com>
2024-06-23 18:28:19 +0330

    feat :validationSchema

--- src/validation/validationSchema.ts
+++ src/validation/validationSchema.ts
@@ -5,3 +5,14 @@ export const schemaLogin = z.object({
   username: z.string().min(1, inValidMsgLogin.username.min),
   password: z.string().min(1, inValidMsgLogin.password.min),
 });
+export const schemaForgotPage = z.object({
+  email: z.string().email(inValidMsgForgot.email.email),
+});
+export const schemaReset = z
+  .object({
+    password: z.string(),
+    confirm: z.string(),
+  })
+  .refine((data) => data.password === data.confirm, {
+    message: "Passwords don't match",
+  });
