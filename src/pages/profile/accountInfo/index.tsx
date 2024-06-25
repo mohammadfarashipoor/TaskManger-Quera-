0935fd66ce0db79620e2b9680453d3ec949ec2cf
mr-rahimi900 <mrrahimi.frontend@gmail.com>
2024-06-25 19:38:23 +0330

    feat(accountInfo) : add form , input , button

--- src/pages/profile/accountInfo/index.tsx
+++ src/pages/profile/accountInfo/index.tsx
@@ -1,5 +1,35 @@
+import { Button } from "@/ui/atom/Button";
+import InputText from "@/ui/atom/InputText";
+import FormContainer from "@/ui/molocol/FormContainer";
+
+interface AccountData{
+  email:string;
+  username:string;
+  password:string;
+
+}
+
 function AccountInfoPage() {
-  return <div>AccountInfo</div>;
+
+  const onSubmit = (data:AccountData) => {
+    console.log("data" , data);
+  };
+
+  return(
+    <div>
+    <FormContainer onSubmit={onSubmit} schema>
+        <h2>
+         اطلاعات حساب
+        </h2>
+          <InputText name="email" label="ایمیل" classNames="mb-4" />
+          <InputText name="username" label="نام کاربری" classNames="mb-4" />
+          <InputText type="password" name="password" label="رمز عبور فعلی" classNames="mb-2" />
+          <InputText type="password" name="password" label="رمز عبور جدید" classNames="mb-2" />
+          <InputText type="password" name="password" label="تکرار رمز عبور جدید" classNames="mb-2" />
+          <Button className="mt-6" type="submit" isBold isprimary>ثبت تغییرات</Button>
+      </FormContainer>
+  </div>
+  );
 }
 
 export default AccountInfoPage;
