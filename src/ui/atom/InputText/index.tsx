14044f12eaae906ee74fc97c3ab67e8a9a353776
Mohammad <mohammad1f2f@gmail.com>
2024-06-25 11:03:06 +0330

    fix : inline element in Input

--- src/ui/atom/InputText/index.tsx
+++ src/ui/atom/InputText/index.tsx
@@ -6,28 +6,44 @@ interface InputTextProps {
   label?: string;
   classNameLabel?: string;
   type?: "text" | "number" | "password";
+  inlineElement?: React.ReactNode;
 }
 
 const InputText: React.FC<InputTextProps> = (props) => {
-  const { name, label, classNames, classNameLabel, type = "text" } = props;
+  const {
+    name,
+    label,
+    classNames,
+    classNameLabel,
+    type = "text",
+    inlineElement,
+  } = props;
   const {
     register,
     formState: { errors },
   } = useFormContext();
+
   return (
-    <div className="flex flex-col items-start gap-xs self-stretch">
+    <div className="flex flex-col items-start gap-xs p-2">
       {label && (
         <label className={classNameLabel} htmlFor={name}>
           {label}
         </label>
       )}
-      <input
-        id={name}
-        type={type}
-        className={`p-xs self-stretch rounded-[6px] border border-gray-400 h-xl ${classNames} `}
-        {...register(name)}
-      />
-      {errors[name] && <span>{errors[name].message}</span>}
+      <div className="flex">
+        <input
+          id={name}
+          type={type}
+          className={`p-xs self-stretch ${
+            inlineElement ? "rounded-r-[6px]" : "rounded-[6px]"
+          } border border-gray-400 h-xl ${classNames} `}
+          {...register(name)}
+        />
+        {inlineElement && inlineElement}
+      </div>
+      {errors[name] && (
+        <span className="text-red-600 mb-1">{errors[name].message}</span>
+      )}
     </div>
   );
 };
