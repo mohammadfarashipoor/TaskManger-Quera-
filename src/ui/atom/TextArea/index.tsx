5f628b9aba38bbc253490074cad31214a0cb2834
mr-rahimi900 <mrrahimi.frontend@gmail.com>
2024-06-28 22:05:58 +0330

    feat:apply TextArea in atoms

--- src/ui/atom/TextArea/index.tsx
+++ src/ui/atom/TextArea/index.tsx
@@ -4,17 +4,22 @@ interface TextAreaProps{
     name:string;
     classNames?:string;
     value?:string;
+    rowsNumber:number;
+    columnsNumber:number;
 }
 
 const TextArea:React.FC<TextAreaProps>=(props)=> {
 
-    const{name,value,classNames}=props
+    const{name,value,classNames,rowsNumber,columnsNumber}=props
 
   return (
     <div className="flex w-full">
-        <textarea id={name} className={`w-full rounded-[12px] border border-gray-400 p-xs ${classNames}`} defaultValue={value}></textarea>
+        <textarea id={name} rows={rowsNumber} cols={columnsNumber} className={`w-full rounded-[12px] border border-gray-400 p-xs ${classNames}`} defaultValue={value}></textarea>
     </div>
   )
 }
 
 export default TextArea
+
+
+          {/* <TextArea name="message" value="توضیحاتی برای این تسک بنویسید" rowsNumber={5} columnsNumber={150}/> */}
