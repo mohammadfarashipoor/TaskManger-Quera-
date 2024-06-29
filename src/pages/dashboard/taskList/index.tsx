a5a4f279a99b67ac67f63b51ccb20cc2d96461be
Mohammad <mohammad1f2f@gmail.com>
2024-06-29 18:11:44 +0330

    feat : TaskList page

--- src/pages/dashboard/taskList/index.tsx
+++ src/pages/dashboard/taskList/index.tsx
@@ -1,4 +1,16 @@
+import Dropdown from "@/ui/atom/Dropdown";
+import Text from "@/ui/atom/typography/Text";
+import TaskList from "@/ui/organism/TaskList";
+
 function TaskListViewPage() {
-  return <div>TaskListView</div>;
+  return (
+    <Dropdown Header={<Text textSize="boldXL">پروژه اول</Text>}>
+      <TaskList status="Pending" className="mr-[25px] mt-5" />
+      <TaskList status="Open" className="mr-[25px] mt-5" />
+      <TaskList status="InProgress" className="mr-[25px] mt-5" />
+      <TaskList status="ToDO" className="mr-[25px] mt-5" />
+      <TaskList status="Done" className="mr-[25px] mt-5" />
+    </Dropdown>
+  );
 }
 export default TaskListViewPage;
--- src/ui/atom/TaskStatus/statusColor.ts
+++ src/ui/atom/TaskStatus/statusColor.ts
@@ -2,22 +2,22 @@ export const statusColor = {
   Open: {
     name: "Open",
     borderColor: "border-orange-primary",
-    bg: "bg-green-primary",
+    bg: "bg-orange-primary",
   },
   InProgress: {
     name: "In Progress",
     borderColor: "border-blue-primary",
-    bg: "bg-green-primary",
+    bg: "bg-blue-primary",
   },
   Pending: {
     name: "Pending",
     borderColor: "border-yellow-primary",
-    bg: "bg-green-primary",
+    bg: "bg-yellow-primary",
   },
   ToDO: {
     name: "To DO",
     borderColor: "border-orange-secondary",
-    bg: "bg-green-primary",
+    bg: "bg-orange-primary",
   },
   Done: {
     name: "Done",
