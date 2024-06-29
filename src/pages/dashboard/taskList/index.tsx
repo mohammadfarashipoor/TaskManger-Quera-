4f5097b0d663cf222083129720420eeaa3729f3c
Mohammad <mohammad1f2f@gmail.com>
2024-06-29 18:21:47 +0330

    refactor : TaskList page

--- src/pages/dashboard/taskList/index.tsx
+++ src/pages/dashboard/taskList/index.tsx
@@ -1,16 +1,29 @@
+import { Button } from "@/ui/atom/Button";
 import Dropdown from "@/ui/atom/Dropdown";
 import Text from "@/ui/atom/typography/Text";
 import TaskList from "@/ui/organism/TaskList";
 
 function TaskListViewPage() {
   return (
-    <Dropdown Header={<Text textSize="boldXL">پروژه اول</Text>}>
-      <TaskList status="Pending" className="mr-[25px] mt-5" />
-      <TaskList status="Open" className="mr-[25px] mt-5" />
-      <TaskList status="InProgress" className="mr-[25px] mt-5" />
-      <TaskList status="ToDO" className="mr-[25px] mt-5" />
-      <TaskList status="Done" className="mr-[25px] mt-5" />
-    </Dropdown>
+    <div className="overflow-y-scroll relative h-[90vh] p-6">
+      <Dropdown Header={<Text textSize="boldXL">پروژه اول</Text>}>
+        <TaskList status="Pending" className="mr-[25px] mt-5" />
+        <TaskList status="Open" className="mr-[25px] mt-5" />
+        <TaskList status="InProgress" className="mr-[25px] mt-5" />
+        <TaskList status="ToDO" className="mr-[25px] mt-5" />
+        <TaskList status="Done" className="mr-[25px] mt-5" />
+      </Dropdown>
+      <Dropdown Header={<Text textSize="boldXL">پروژه اول</Text>}>
+        <TaskList status="Pending" className="mr-[25px] mt-5" />
+        <TaskList status="Open" className="mr-[25px] mt-5" />
+        <TaskList status="InProgress" className="mr-[25px] mt-5" />
+        <TaskList status="ToDO" className="mr-[25px] mt-5" />
+        <TaskList status="Done" className="mr-[25px] mt-5" />
+      </Dropdown>
+      <Button iconName="add" className="fixed bottom-5 left-5 w-32">
+        تسک جدید
+      </Button>
+    </div>
   );
 }
 export default TaskListViewPage;
