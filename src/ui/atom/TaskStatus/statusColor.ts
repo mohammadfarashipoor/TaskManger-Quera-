3afdd86532bf0fe13a132c0a4729a574b5ed4f45
Mohammad <mohammad1f2f@gmail.com>
2024-06-29 13:34:48 +0330

    feat : TaskStatus atom

---
+++ src/ui/atom/TaskStatus/statusColor.ts
@@ -0,0 +1,27 @@
+export const statusColor = {
+  Open: {
+    name: "Open",
+    borderColor: "border-orange-primary",
+    bg: "bg-green-primary",
+  },
+  InProgress: {
+    name: "In Progress",
+    borderColor: "border-blue-primary",
+    bg: "bg-green-primary",
+  },
+  Pending: {
+    name: "Pending",
+    borderColor: "border-yellow-primary",
+    bg: "bg-green-primary",
+  },
+  ToDO: {
+    name: "To DO",
+    borderColor: "border-orange-secondary",
+    bg: "bg-green-primary",
+  },
+  Done: {
+    name: "Done",
+    borderColor: "border-green-primary",
+    bg: "bg-green-primary",
+  },
+};
