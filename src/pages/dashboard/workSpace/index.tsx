d0b1d3c495c211082835532aad48abd81632768a
mr-rahimi900 <mrrahimi.frontend@gmail.com>
2024-07-02 13:13:37 +0330

    feat : workspacemain molcol

--- src/pages/dashboard/workSpace/index.tsx
+++ src/pages/dashboard/workSpace/index.tsx
@@ -1,12 +1,8 @@
-import { Card } from "@/ui/atom/Card";
-import { TitleCard } from "@/ui/atom/TitleCard";
+import { WorkSpaceMain } from "@/ui/molocol/WorkspaceMain";
 
 function WorkSpacePage() {
   return <div>
-    <TitleCard>
-      درس مدیریت پروژه
-    </TitleCard>
-    <Card className="bg-gradient-to-br from-green-300 via-green-500 to-white-200">پروژه اول</Card>
+    <WorkSpaceMain/>
   </div>;
 }
 
